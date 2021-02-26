a89fc72c5e6679a0d3abaaabcd69339b44c038df
teddydeddy123 <69483384+teddydeddy123@users.noreply.github.com>
2021-02-26 17:40:46 +0200

    Create website_idea1.js

---
+++ website_idea1.js
@@ -0,0 +1,37 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('portfolio__img');
+var images = document.getElementsByClassName('portfolio__img1');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
